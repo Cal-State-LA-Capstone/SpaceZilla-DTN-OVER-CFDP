a676658e6ea325f71c9bbca9f1778dc9fa37c653
AngelG-CSULA <agayta29@calstatela.edu>
2026-03-11 23:35:19 -0700

    Add test app, frontend and backend, and the docker imagefile that creates a container with a working pyion env

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from backend.test_backend import TestBackend
+from frontend.test_frontend import TestUI
+
+def main():
+    backend = TestBackend(ipn='1.1', node='1')
+    ui = TestUI(backend)
+    ui.run()
+
+if __name__ == "__main__":
+    main()
